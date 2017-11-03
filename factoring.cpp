045b10074c581271e64270d2613464911c171e88
KanskeMarcus <mlig@kth.se>
2017-11-03 10:23:37 +0100

    funkar inte

---
+++ factoring.cpp
@@ -0,0 +1,32 @@
+//Author Marcus Lignercrona & Emelie Eriksson
+#include <iostream>
+#include <string>
+class factoring{
+	public:
+		int divisor2(long long & number){
+			int temp;
+			while(true){
+				temp = number & 1;
+				if(temp == 0){
+					number>>=1;
+					std::cout << 2 << "\n";
+				}
+				else{
+					break;
+				}
+			}
+		}
+};
+	int main (){
+		long long a;
+		std::cin >> a;
+		while(1){
+			std::cout << "fail";
+			if(std::cin >> a){
+				std::cout << "\n" <<"\n";
+			}
+			else{
+				break;
+			}
+		}
+	}
